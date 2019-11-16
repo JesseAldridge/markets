f4ae079b94248cf9417fd42444a92934e0ea1f2c
Jesse Aldridge <jessealdridge@gmail.com>
2019-11-16 00:35:08 -0800

    new graph

--- _2_global_gdp_by_industry_inflation.py
+++ _2_global_gdp_by_industry_inflation.py
@@ -1,18 +1,19 @@
 import _0_parse_oecd
 
 # Global GDP by sector
-sector_info = _0_parse_oecd.get_sector_by_year_by_country_to_gdp()
-sector_by_year_by_country_to_gdp = sector_info.sector_by_year_by_country_to_gdp
-years = range(sector_info.min_year, sector_info.max_year)
+sector_info = _0_parse_oecd.SectorInfo()
+sector_by_country_by_year_to_gdp = sector_info.sector_by_country_by_year_to_gdp
+years = range(1980, sector_info.max_year + 1)
 countries = sector_info.countries
 
 year_to_cpi = _0_parse_oecd.get_year_to_cpi()
 with open('gdp-per-sector-infl-adjusted.csv', 'w') as f:
   f.write(','.join(['sector'] + [str(year) for year in years]) + '\n')
-  for sector in sector_by_year_by_country_to_gdp:
+  for sector in sector_by_country_by_year_to_gdp:
+    country_by_year_to_gdp = sector_by_country_by_year_to_gdp[sector]
     val_strs = []
-    year_by_country_to_gdp = sector_by_year_by_country_to_gdp[sector]
-    for year in year_to_cpi:
+    for year in years:
+      gdp = sum(country_by_year_to_gdp[country][year] for country in country_by_year_to_gdp)
       cpi = year_to_cpi[year]
-      val_strs.append(str(sum(year_by_country_to_gdp[year].values()) / (cpi / 100)))
+      val_strs.append(str(gdp / (cpi / 100)))
     f.write('"{}",{}\n'.format(sector, ','.join(val_strs)))
